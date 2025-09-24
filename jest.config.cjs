271123757a8680b296c463b749670a4e5f00ab7f
CClassics <149358294+AuZanPs@users.noreply.github.com>
2025-09-24 22:10:55 +0700

    Update Jest configuration for better test matching and module resolution

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,13 +2,30 @@
 module.exports = {
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }]
+    '^.+\\.(ts|tsx)$': ['ts-jest', { 
+      tsconfig: 'tsconfig.json',
+      useESM: true 
+    }]
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
-  testMatch: ['**/src/tests/**/*.test.tsx'],
-  globals: {},
-  moduleNameMapper: {
-    '\\.(css|less|scss)$': '<rootDir>/src/test/styleMock.js'
-  }
+  testMatch: [
+    '**/src/tests/**/*.test.{ts,tsx}',
+    '**/src/**/__tests__/**/*.{ts,tsx}',
+    '**/src/**/*.{test,spec}.{ts,tsx}'
+  ],
+  moduleNameMapping: {
+    '\\.(css|less|scss)$': '<rootDir>/src/test/styleMock.js',
+    '^src/(.*)$': '<rootDir>/src/$1'
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/test/**',
+    '!src/main.tsx'
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(.*\\.mjs$))'
+  ]
 };
